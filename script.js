89c33be5b61ea8b9537d63440a7d83722ed21322
PharmaAsmaa <asmasyria2002@gmail.com>
2025-10-03 17:33:47 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,119 @@
-// Her setter du alle ressurser (samme som tidligere)
-const ressurser = { /* ... kopier alle mapper og pdf-lister her ... */ };
+const ressurser = {
+  "Vaksinasjon": [
+    { name: "Registrering av vaksiner.pdf", path: "Vaksinasjon/Aposys registrering av vaksiner.pdf" }
+  ],
+  "Viktige lover og forskrifter": [
+    { name: "Bransjestandard for bruksanvisningsetikett i Apotek.pdf", path: "Viktige lover og forskrifter/Bransjestandard for bruksanvisningsetikett i Apotek.pdf" },
+    { name: "Kjøring ved legemiddelbehandling.pdf", path: "Viktige lover og forskrifter/Kjøring ved legemiddelbehandling.pdf" },
+    { name: "LAR - Legemiddelassistert rehabilitering i apotek.pdf", path: "Viktige lover og forskrifter/LAR - Legemiddelassistert rehabilitering i apotek.pdf" },
+    { name: "Mottak og istandgjøring av resept.pdf", path: "Viktige lover og forskrifter/Mottak og istandgjøring av resept.pdf" },
+    { name: "Refusjonsordninger for legemidler.pdf", path: "Viktige lover og forskrifter/Refusjonsordninger for legemidler.pdf" }
+  ],
+  "Aposys": {
+    "Registrering av inhalasjonsveiledning og medisinstartsamtalen": [
+      { name: "Registrering av inhalasjonsveiledning.pdf", path: "Aposys/Inhalasjonsveiledning&Medisinstart/Registrering av inhalasjonsveiledning.pdf" },
+      { name: "Registrering av medisinstartsamtalen.pdf", path: "Aposys/Inhalasjonsveiledning&Medisinstart/Registrering av medisinstart.pdf" }
+    ],
+    "Kundeportalen (Orderkunder)": [
+      { name: "Kundeportalen (Orderkunder).pdf", path: "Aposys/Kundeportalen/Kundeportalen (Orderkunder).pdf" }
+    ],
+    "Reseptur": [
+      { name: "E-resept.pdf", path: "Aposys/Reseptur/E-resept.pdf" },
+      { name: "Intervensjon&Legemiddelmangel.pdf", path: "Aposys/Reseptur/Intervensjon&Legemiddelmangel.pdf" },
+      { name: "Kundehistorikk.pdf", path: "Aposys/Reseptur/Kundehistorikk.pdf" },
+      { name: "Kundesøk.pdf", path: "Aposys/Reseptur/Kundesøk.pdf" },
+      { name: "Papirresept.pdf", path: "Aposys/Reseptur/Papirresept.pdf" },
+      { name: "Produktgruppe resept.pdf", path: "Aposys/Reseptur/Produktgruppe resept.pdf" },
+      { name: "Skannresept.pdf", path: "Aposys/Reseptur/Skannresept.pdf" },
+      { name: "Veterinærresept.pdf", path: "Aposys/Reseptur/Veterinærresept.pdf" }
+    ],
+    "Kasse": [
+      { name: "Anbrudd.pdf", path: "Aposys/Kasse/Anbrudd.pdf" },
+      { name: "Detaljert Kvittering.pdf", path: "Aposys/Kasse/Detaljert Kvittering.pdf" },
+      { name: "Hente en ferdig resept over til kassen.pdf", path: "Aposys/Kasse/Hente en ferdig resept over til kassen.pdf" },
+      { name: "Retur.pdf", path: "Aposys/Kasse/Retur.pdf" },
+      { name: "Søke etter vare uten scann.pdf", path: "Aposys/Kasse/Søke etter vare uten scann.pdf" },
+      { name: "VI-medlem.pdf", path: "Aposys/Kasse/VI-medlem.pdf" }
+    ]
+  },
+  "Veiledning i apotek": {
+    "Autoimmune&Relaterte": [
+      { name: "Stomi.pdf", path: "Veiledning i apotek/Autoimmune&Relaterte/Stomi.pdf" }
+    ],
+    "Stoffskiftesykdommer": [
+      { name: "Stoffskiftesykdommer.pdf", path: "Veiledning i apotek/Stoffskiftesykdommer/Stoffskiftesykdommer.pdf" }
+    ],
+    "Plager forårsaket av parasitter, orm & innsekter": [
+      { name: "Flåttbitt.pdf", path: "Veiledning i apotek/Plager forårsaket av parasitter, orm & innsekter/Flottbitt.pdf" },
+      { name: "Hoggormbitt.pdf", path: "Veiledning i apotek/Plager forårsaket av parasitter, orm & innsekter/Hoggormbitt.pdf" },
+      { name: "Innvollsorm (Barnemark).pdf", path: "Veiledning i apotek/Plager forårsaket av parasitter, orm & innsekter/Innvollsorm (Barnemark).pdf" },
+      { name: "Insektstikk.pdf", path: "Veiledning i apotek/Plager forårsaket av parasitter, orm & innsekter/Insektstikk.pdf" },
+      { name: "Loppebitt.pdf", path: "Veiledning i apotek/Plager forårsaket av parasitter, orm & innsekter/Loppebitt.pdf" },
+      { name: "Lusebitt.pdf", path: "Veiledning i apotek/Plager forårsaket av parasitter, orm & innsekter/Lusebitt.pdf" },
+      { name: "Skabb.pdf", path: "Veiledning i apotek/Plager forårsaket av parasitter, orm & innsekter/Skabb.pdf" }
+    ],
+    "Dermatosis (Hudlidelser)": [
+      { name: "Atopisk Eksem.pdf", path: "Veiledning i apotek/Dermatosis (Hudlidelser)/Atopisk Eksem.pdf" },
+      { name: "Brennkoper (Impetigo).pdf", path: "Veiledning i apotek/Dermatosis (Hudlidelser)/Brennkoper (Impetigo).pdf" },
+      { name: "Brennmanteksposisjon.pdf", path: "Veiledning i apotek/Dermatosis (Hudlidelser)/Brennmanteksposisjon.pdf" },
+      { name: "Brosjyre utslett 2022.pdf", path: "Veiledning i apotek/Dermatosis (Hudlidelser)/Brosjyre utslett 2022.pdf" },
+      { name: "Eksemhefte.pdf", path: "Veiledning i apotek/Dermatosis (Hudlidelser)/Eksemhefte.pdf" },
+      { name: "Elveblest (Urticaria).pdf", path: "Veiledning i apotek/Dermatosis (Hudlidelser)/Elveblest (Urticaria).pdf" },
+      { name: "Flass (Pityriasis Capitis).pdf", path: "Veiledning i apotek/Dermatosis (Hudlidelser)/Flass (Pityriasis Capitis).pdf" },
+      { name: "Fotsopp (Tinea Pedis).pdf", path: "Veiledning i apotek/Dermatosis (Hudlidelser)/Fotsopp (Tinea Pedis).pdf" },
+      { name: "Kviser (Akne).pdf", path: "Veiledning i apotek/Dermatosis (Hudlidelser)/Kviser (Akne).pdf" },
+      { name: "Munnsår (Herpes Labialis).pdf", path: "Veiledning i apotek/Dermatosis (Hudlidelser)/Munnsor (Herpes Labialis).pdf" },
+      { name: "Solforbrenning.pdf", path: "Veiledning i apotek/Dermatosis (Hudlidelser)/Solforbrenning.pdf" },
+      { name: "Vorter (Verruca Vulgaris).pdf", path: "Veiledning i apotek/Dermatosis (Hudlidelser)/Vorter (Verruca Vulgaris).pdf" }
+    ],
+    "Infertilitetsbehandling, Antikonsepsjon & Hormonerstatning": [
+      { name: "Antikonsepsjon.pdf", path: "Veiledning i apotek/Infertilitetsbehandling, Antikonsepsjon & Hormonerstatning/Antikonsepsjon.pdf" },
+      { name: "Infertilitetsbehandling.pdf", path: "Veiledning i apotek/Infertilitetsbehandling, Antikonsepsjon & Hormonerstatning/Infertilitetsbehandling.pdf" },
+      { name: "Overgangsalderen.pdf", path: "Veiledning i apotek/Infertilitetsbehandling, Antikonsepsjon & Hormonerstatning/Overgangsalderen.pdf" }
+    ],
+    "Hjerte-og karsykdommer": [
+      { name: "Blodfortynnende behandling.pdf", path: "Veiledning i apotek/Hjerte-og karsykdommer/Blodfortynnende behandling.pdf" },
+      { name: "Hjertesvikt.pdf", path: "Veiledning i apotek/Hjerte-og karsykdommer/Hjertesvikt.pdf" },
+      { name: "Hypertensjon.pdf", path: "Veiledning i apotek/Hjerte-og karsykdommer/Hypertensjon.pdf" },
+      { name: "Iskemisk hjertesykdom.pdf", path: "Veiledning i apotek/Hjerte-og karsykdommer/Iskemisk hjertesykdom.pdf" },
+      { name: "Kolesterolsenkende behandling.pdf", path: "Veiledning i apotek/Hjerte-og karsykdommer/Kolesterolsenkende behandling.pdf" },
+      { name: "Overvekt&Fedme.pdf", path: "Veiledning i apotek/Hjerte-og karsykdommer/Overvekt&Fedme.pdf" }
+    ],
+    "CNS-lidelser": [
+      { name: "ADHD.pdf", path: "Veiledning i apotek/CNS-lidelser/ADHD.pdf" },
+      { name: "Angst.pdf", path: "Veiledning i apotek/CNS-lidelser/Angst.pdf" },
+      { name: "Demens.pdf", path: "Veiledning i apotek/CNS-lidelser/Demens.pdf" },
+      { name: "Depresjon.pdf", path: "Veiledning i apotek/CNS-lidelser/Depresjon.pdf" },
+      { name: "Epilepsi.pdf", path: "Veiledning i apotek/CNS-lidelser/Epilepsi.pdf" },
+      { name: "Farmakoterapi hos gravide&ammende.pdf", path: "Veiledning i apotek/CNS-lidelser/Farmakoterapi hos gravide&ammende.pdf" },
+      { name: "Kvalme&Oppkast.pdf", path: "Veiledning i apotek/CNS-lidelser/Kvalme&Oppkast.pdf" },
+      { name: "Migrene.pdf", path: "Veiledning i apotek/CNS-lidelser/Migrene.pdf" },
+      { name: "Parkinsons sykdom.pdf", path: "Veiledning i apotek/CNS-lidelser/Parkinsons sykdom.pdf" },
+      { name: "Schizofreni.pdf", path: "Veiledning i apotek/CNS-lidelser/Schizofreni.pdf" },
+      { name: "Smerter.pdf", path: "Veiledning i apotek/CNS-lidelser/Smerter.pdf" },
+      { name: "Søvnvansker.pdf", path: "Veiledning i apotek/CNS-lidelser/Søvnvansker.pdf" }
+    ],
+    "Hudpleie": [
+      { name: "02 Digital Hudsamling - Dermalogica Anti-age.pdf", path: "Veiledning i apotek/Hudpleie/02 Digital Hudsamling - Dermalogica Anti-age.pdf" },
+      { name: "03 Digital Hudsamling - Vichy Anti-age.pdf", path: "Veiledning i apotek/Hudpleie/03 Digital Hudsamling - Vichy Anti-age.pdf" },
+      { name: "04 Digital Hudsamling - Eucerin Anti-age.pdf", path: "Veiledning i apotek/Hudpleie/04 Digital Hudsamling - Eucerin Anti-age.pdf" },
+      { name: "05 Digital Hudsamling - Avene Anti-age.pdf", path: "Veiledning i apotek/Hudpleie/05 Digital Hudsamling - Avene Anti-age.pdf" },
+      { name: "06 Digital Hudsamling - Klaris.pdf", path: "Veiledning i apotek/Hudpleie/06 Digital Hudsamling - Klaris.pdf" },
+      { name: "07 Digital Hudsamling Akne.pdf", path: "Veiledning i apotek/Hudpleie/07 Digital Hudsamling Akne.pdf" }
+    ],
+    "Selvvalg": [
+      { name: "Kateter.pdf", path: "Veiledning i apotek/Selvvalg/Kateter.pdf" },
+      { name: "Sårbehandling.pdf", path: "Veiledning i apotek/Selvvalg/Sorbehandling.pdf" },
+      { name: "Tannpine(Dentalgia) & Tannframbrudd(Dentitio).pdf", path: "Veiledning i apotek/Selvvalg/Tannpine(Dentalgia)&Tannframbrudd(Dentitio).pdf" }
+    ],
+    "Lungesykdommer": [
+      { name: "Astma&Kols.pdf", path: "Veiledning i apotek/Lungesykdommer/Astma&Kols.pdf" }
+    ],
+    "Veterinærmedisin": [],
+    "Antibiotika": []
+  }
+};
+
 
 // Sjekk passord
 function sjekkPassord() {
